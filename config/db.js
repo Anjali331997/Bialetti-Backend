5a48381183ea82e0d6c0336573f4756f8740e071
Anjali331997 <anjaliap3march1997@gmail.com>
2023-08-16 15:24:33 +0530

    nodemon removed

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 require("dotenv").config()
 
 
-const connection=mongoose.connect(`mongodb+srv://anjaliap3march1997:anjali03031997ap@cluster0.qw0epzc.mongodb.net/?retryWrites=true&w=majority`)
+const connection=mongoose.connect(process.env.MONOGO_URL)
 
 module.exports ={connection}
