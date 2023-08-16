2432a02cc26d5c6a53d213a1d76372085398553b
Anjali331997 <anjaliap3march1997@gmail.com>
2023-08-16 18:18:18 +0530

    locathost link working

--- Authentication/authentication.js
+++ Authentication/authentication.js
@@ -1,10 +1,11 @@
 var jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+
 const authentication = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1]
     if (token) {
-        jwt.verify(token, process.env.SECRET_KEY , function (err, decoded) {
+        jwt.verify(token, process.env.SECRET_KEY  , function (err, decoded) {
             if (err) {
                 res.status(200).json({ msg: "token not valid" })
             } else {
@@ -13,6 +14,7 @@ const authentication = (req, res, next) => {
                 next()
             }
         })
+        
     } else {
         res.send({message:"Invalid credentials"})
     }
