79338e1469b6524f689109659df775dea7fe101d
Anjali331997 <anjaliap3march1997@gmail.com>
2023-08-18 21:42:07 +0530

    Backend bialeti trying again

--- config/db.js
+++ config/db.js
@@ -1,10 +1,4 @@
 const mongoose = require('mongoose')
-const express = require('express');
-var cors = require('cors');
-const app = express();
-app.use(cors({
-    origin: "*"
-}))
 require("dotenv").config()
 
 
