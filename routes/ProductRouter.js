e49e327c1f2565deb1913a3b635c7872d543112c
Anjali331997 <anjaliap3march1997@gmail.com>
2024-04-24 22:39:51 +0530

    resolve error in product route try 9

--- routes/ProductRouter.js
+++ routes/ProductRouter.js
@@ -4,18 +4,21 @@ const { ProductModel } = require("../models/product.model.js");
 
 const ProductRouter = express.Router();
 
-ProductRouter.get('/test',(req,res)=>{
-    res.send({message:"products routes are working"})
+ProductRouter.get('/test', (req, res) => {
+    res.send({ message: "products routes are working" })
 })
 
 ProductRouter.get("/", async (req, res) => {
     try {
         const allProducts = await ProductModel.find({});
         // console.log(allProducts);
-        res.status(200).send({allProducts});
-       
+        res.status(200).send({ allProducts });
+
     } catch (error) {
-        res.status(500).send({msg : "Error getting all Products data",err:"herr"})
+        res.status(500).send({
+            "msg": "Error getting all Products data",
+            "err": error
+        })
     }
 })
 
